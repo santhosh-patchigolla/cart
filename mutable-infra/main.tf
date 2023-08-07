43e04b84efa55876db15d3ef6bd71e64b01fbae4
patchigolla santhosh <santhosh.ramana96@gmail.com>
2023-08-07 17:03:33 +0530

    sdc

--- mutable-infra/main.tf
+++ mutable-infra/main.tf
@@ -1,35 +1,7 @@
-module "docdb" {
-  source                 = "./vendor/modules/docdb"
+module "cart" {
+  source                 = "./vendor/modules/app"
   ENV                    = var.ENV
-  DOCDB_PORT_NUMBER      = var.DOCDB_PORT_NUMBER
-  DOCDB_INSTANCE_TYPE    = var.DOCDB_INSTANCE_TYPE 
-  DOCDB_INSTANCE_COUNT   = var.DOCDB_INSTANCE_COUNT
-}
-
-module "redis" {
-  source                 = "./vendor/modules/redis"
-  ENV                    = var.ENV
-  REDIS_PORT_NUMBER      = var.REDIS_PORT_NUMBER
-  REDIS_INSTANCE_TYPE    = var.REDIS_INSTANCE_TYPE
-  REDIS_INSTANCE_COUNT   = var.REDIS_INSTANCE_COUNT
-  REDIS_ENGINE_VERSION   = var.REDIS_ENGINE_VERSION
-
-}
-module "mysql" {
-  source                 = "./vendor/modules/mysql"
-  ENV                    = var.ENV
-  MYSQL_PORT_NUMBER      = var.MYSQL_PORT_NUMBER
-  MYSQL_STORAGE          = var.MYSQL_STORAGE 
-  MYSQL_ENGINE_VERSION   = var.MYSQL_ENGINE_VERSION
-  MYSQL_INSTANCE_TYPE    = var.MYSQL_INSTANCE_TYPE
-}
-
-
-module "rabbitmq" {
-  source                 = "./vendor/modules/rabbitmq"
-  ENV                    = var.ENV
-  RABBITMQ_PORT_NUMBER   = var. RABBITMQ_PORT_NUMBER
-  RABBITMQ_INSTANCE_TYPE = var.RABBITMQ_INSTANCE_TYPE
+  COMPONENT              = var.COMPONENT
 }
 
 # We are passing the variable in this way..
